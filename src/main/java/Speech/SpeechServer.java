1bffe1501c8c30b554f0e422bba0a52b49e16dd6
Darshan Patil <dapatil211@gmail.com>
2015-10-03 16:56:29 -0700

    Added basic server structure

--- src/main/java/Speech/SpeechServer.java
+++ src/main/java/Speech/SpeechServer.java
@@ -1,4 +1,14 @@
 package Speech;
-public class SpeechServer {
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class SpeechServer {
+	public static void main(String[] args) throws Exception{
+		Server server = new Server(8080);
+		ServletContextHandler handler = new ServletContextHandler(server, "/");
+		handler.addServlet(RecordServlet.class, "/");
+		server.setHandler(handler);
+		server.start();
+	}
 }
