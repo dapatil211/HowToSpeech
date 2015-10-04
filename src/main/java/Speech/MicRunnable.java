4f0eb53f0174e7c59fa5983e3208e69a787019eb
Darshan Patil <dapatil211@gmail.com>
2015-10-03 23:57:27 -0700

    Added streaming of sound data to watson

--- src/main/java/Speech/MicRunnable.java
+++ src/main/java/Speech/MicRunnable.java
@@ -1,8 +1,12 @@
 package Speech;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.sound.sampled.AudioFileFormat;
@@ -22,34 +26,59 @@ public class MicRunnable implements Runnable {
 	private static final String WATSON_USERNAME = "21d3a7d1-b2c5-471b-a66c-d20885130dda";
 
 	TargetDataLine microphone = null;
-	public void finish(){
+
+	public void finish() {
 		microphone.stop();
 		microphone.close();
 	}
-	public void run() {
-		AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
-		try {
-			microphone = AudioSystem.getTargetDataLine(format);
-		} catch (LineUnavailableException e) {
-			System.err.println(e.getMessage());
-		}
 
-		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format); 
+	public void run() {
 		try {
+			List<SpeechResults> transcripts = new ArrayList<SpeechResults>();
+			AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+			microphone = AudioSystem.getTargetDataLine(format);
+
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 			microphone.open(format);
-		} catch (LineUnavailableException ex) {
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] data = new byte[8000 * format.getFrameSize()];
+			AudioInputStream inputStream = new AudioInputStream(microphone);
+
+			microphone.start();
+			SpeechToText service = new SpeechToText();
+			service.setUsernameAndPassword(WATSON_USERNAME, WATSON_PASSWORD);
+			SpeechSession session = service.createSession();
+			int offset = 0;
+			while (true) {
+				int numBytesRead = inputStream.read(data);
+				if (numBytesRead == -1) {
+					break;
+				}
+				out.write(data);
+				if (offset == 14) {
+					offset = -1;
+					AudioInputStream outInputStream = new AudioInputStream(
+							new ByteArrayInputStream(out.toByteArray()),
+							format, out.size() / format.getFrameSize());
+					File fileOut = new File("./watson/sound.wav");
+					AudioSystem.write(outInputStream,
+							AudioFileFormat.Type.WAVE, fileOut);
+					SpeechResults transcript = textToSpeech(service, fileOut);
+					transcripts.add(transcript);
+					System.out.println(transcript);
+				}
+				offset++;
+			}
+		} catch (IOException ex) {
 			// Handle the error ...
+		} catch (LineUnavailableException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		SpeechToText service = new SpeechToText();
-		service.setUsernameAndPassword(WATSON_USERNAME, WATSON_PASSWORD);
-		SpeechSession session = service.createSession();
-		AudioInputStream inputStream = new AudioInputStream(microphone);
-		microphone.start();
-		File fileOut = new File("./watson/sound.wav");
-		try {
-			AudioSystem.write(inputStream, AudioFileFormat.Type.WAVE, fileOut);
-		} catch (IOException e) {
-		}
+	}
+
+	private SpeechResults textToSpeech(SpeechToText service, File fileOut) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("audio", fileOut);
 		params.put("content_type", "audio/wav");
@@ -59,7 +88,6 @@ public class MicRunnable implements Runnable {
 		params.put("inactivity_timeout", 30);
 		params.put("max_alternatives", 1);
 		SpeechResults transcript = service.recognize(params);
-
-		System.out.println(transcript);
+		return transcript;
 	}
 }
