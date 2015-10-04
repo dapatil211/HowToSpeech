5f0dbf42cbfb3d4f3562852c5904123bdc5c55ec
Darshan Patil <dapatil211@gmail.com>
2015-10-03 17:28:21 -0700

    Added static files handler

--- src/main/java/Speech/SpeechServer.java
+++ src/main/java/Speech/SpeechServer.java
@@ -1,13 +1,20 @@
 package Speech;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class SpeechServer {
 	public static void main(String[] args) throws Exception{
 		Server server = new Server(8080);
 		ServletContextHandler handler = new ServletContextHandler(server, "/");
-		handler.addServlet(RecordServlet.class, "/");
+		handler.addServlet(RecordServlet.class, "/record");
+		DefaultServlet defaultServlet = new DefaultServlet();
+		ServletHolder holderPwd = new ServletHolder(defaultServlet);
+		holderPwd.setInitParameter("resourceBase", "./src/resources/html");
+
+		handler.addServlet(holderPwd, "/*");
 		server.setHandler(handler);
 		server.start();
 	}
