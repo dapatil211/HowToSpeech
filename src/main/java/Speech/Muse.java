9e03c46e6feb9921d4d67ab37a6f950fbb4a7808
BuildTools <unconfigured@null.spigotmc.org>
2015-10-04 10:05:46 -0700

    Muse.java changed

--- src/main/java/Speech/Muse.java
+++ src/main/java/Speech/Muse.java
@@ -1,5 +1,7 @@
 package Speech;
 
+import java.util.ArrayList;
+
 import oscP5.*;
 
 public class Muse
@@ -8,17 +10,21 @@ public class Muse
 	
 	OscP5 museServer;
 	static int recvPort = 5000;
+	
+	static ArrayList concentrationValues = new ArrayList<Double>();
 
-	public static void main(String [] args) {
+	public static void main(String [] args)
+	{
 		museOscServer = new Muse();
 		museOscServer.museServer = new OscP5(museOscServer, recvPort);
-			}
+	}
 	
 	void oscEvent(OscMessage msg)
 	{
 		if (msg.checkAddrPattern("/muse/elements/experimental/concentration")==true)
-		{  
+		{
 			System.out.print(msg.get(0).floatValue() + "\n");
+			concentrationValues.add(msg.get(0).floatValue());
 		}
 	}
 }
