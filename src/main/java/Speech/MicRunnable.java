6bcc2dc665b622ff49bf0940523ab0225bd3792f
Darshan Patil <dapatil211@gmail.com>
2015-10-03 19:21:30 -0700

    V1 of RecordServlet

--- src/main/java/Speech/MicRunnable.java
+++ src/main/java/Speech/MicRunnable.java
@@ -9,15 +9,17 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
-public class MicTest {
-
-	/**
-	 * @param args
-	 * @throws LineUnavailableException 
-	 */
-	public static void main(String[] args) throws LineUnavailableException {
+public class MicRunnable implements Runnable{
+	public boolean isRunning = true;
+	public void run() {
 		AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
-		TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+		TargetDataLine microphone = null;
+		try {
+			microphone = AudioSystem.getTargetDataLine(format);
+		} catch (LineUnavailableException e) {
+			System.err.println(e.getMessage());
+		}
+		
 		DataLine.Info info = new DataLine.Info(TargetDataLine.class, 
 				format); // format is an AudioFormat object
 		try {
@@ -29,13 +31,13 @@ public class MicTest {
 		int numBytesRead = -1;
 		byte[] data = new byte[microphone.getBufferSize() / 5];
 		microphone.start();
-		while (numBytesRead != 0) {
+		while (isRunning) {
 			   // Read the next chunk of data from the TargetDataLine.
 			   numBytesRead =  microphone.read(data, 0, data.length);
 			   // Save this chunk of data.
 			   out.write(data, 0, numBytesRead);
 			   System.out.println(Arrays.toString(data));
-			}     
+			}
 	}
-}
 
+}
--- src/main/java/Speech/RecordServlet.java
+++ src/main/java/Speech/RecordServlet.java
@@ -1,14 +1,38 @@
 package Speech;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RecordServlet extends HttpServlet{
+import com.google.gson.Gson;
+
+public class RecordServlet extends HttpServlet {
+	Map<Long, MicRunnable> threads = new HashMap<Long, MicRunnable>();
+	private final static Gson gson = new Gson();
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String action = req.getParameter("action");
+		if ("start".equals(action)) {
+			MicRunnable micRunnable = new MicRunnable();
+			Thread micThread = new Thread(micRunnable);
+			micThread.start();
+			threads.put(micThread.getId(), micRunnable);
+			Map<String, Long> retVal = new HashMap<String, Long>();
+			retVal.put("user_id", micThread.getId());
+			resp.getWriter().write(gson.toJson(retVal));
+		}
+		else if("stop".equals(action)){
+			long id = Long.parseLong(req.getParameter("user_id"));
+			threads.get(id).isRunning = false;
+			Map<String, Long> retVal = new HashMap<String, Long>();
+			retVal.put("user_id", id);
+			resp.getWriter().write(gson.toJson(retVal));
+			}
 	}
 }
